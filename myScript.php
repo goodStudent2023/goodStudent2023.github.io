562b244d6b4ab1bccd7aabafbc968e63256b7ac6
goodStudent2023 <134835581+goodStudent2023@users.noreply.github.com>
2023-05-28 23:40:16 +0800

    Add files via upload

---
+++ myScript.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Show database</title>
+    </head>
+
+    <body>
+    <?php
+    $name = "John";
+    $response = "Hello, " . $name . "!";
+    echo $response;
+?>
+    </body>
+</html>
